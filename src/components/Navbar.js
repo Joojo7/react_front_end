43f33fc600ac1eb40cea88e33394a8d3c5be65d7
Joojo Dontoh <joojodontoh@gmail.com>
2020-12-03 23:27:36 +0800

    test change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -57,7 +57,7 @@ function Navbar(user = {}) {
               <Link
                 to="/hotel-rooms"
                 className="nav-links"
-                onClick={closeMobileMenu}
+                onClick={closeMobileMenu} 
               >
                 Hotels
               </Link>
